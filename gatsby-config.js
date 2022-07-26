e393d99d178852ad8c3fe500e99c12c5553b99ff
Zack Norton <norton.zack.w@gmail.com>
2022-07-26 10:30:48 -0500

    Add gatsby-remark-katex for equation formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          'gatsby-remark-katex',
         ],
       },
     },
